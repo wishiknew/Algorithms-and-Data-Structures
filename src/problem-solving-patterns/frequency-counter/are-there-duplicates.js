49a3b4e649498c74fb8f8c8dda2b5f8f7220fbad
wishiknew <wishiknew5@gmail.com>
2021-09-02 21:06:39 +0530

    are there duplicates

--- src/problem-solving-patterns/frequency-counter/are-there-duplicates.js
+++ src/problem-solving-patterns/frequency-counter/are-there-duplicates.js
@@ -4,9 +4,22 @@
 
 // Restrictions: Time Complexity - O(n), Space Complexity - O(n)
 
+function areThereDuplicates1(...args) {
+  const obj = {};
+
+  return args.some((arg) => {
+    if (obj[arg]) return true;
+    obj[arg] = 1;
+  });
+}
+
+console.log(areThereDuplicates1(1, 2, 3, 4));
+
 function areThereDuplicates(...args) {
+  // check if there are args first of all
   if (!args.length) return false;
 
+  // lookup object just like the count object, or a map
   const lookup = {};
 
   for (const item of args) {
@@ -18,4 +31,4 @@ function areThereDuplicates(...args) {
 }
 
 console.log(areThereDuplicates(1, 2, 3)); // false
-console.log(areThereDuplicates('a', 'b', 'c', 'a')); // true
+console.log(areThereDuplicates("a", "b", "c", "a")); // true
