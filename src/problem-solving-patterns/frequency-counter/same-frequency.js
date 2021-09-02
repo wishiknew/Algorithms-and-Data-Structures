9c0b321276dd9cccd0f209be203c78356d22bd83
wishiknew <wishiknew5@gmail.com>
2021-09-02 21:17:35 +0530

    check the frequencies of two numbers are equal

--- src/problem-solving-patterns/frequency-counter/same-frequency.js
+++ src/problem-solving-patterns/frequency-counter/same-frequency.js
@@ -4,14 +4,31 @@
 
 // Your solution MUST have the following complexities: Time: O(N)
 
+function sameFrequency1(num1, num2) {
+  //needs O(n) so I will just have to use for loops
+  // nums are strings?
+  const obj = {};
+  Array.from(String(num1)).forEach((c) => {
+    obj[c] = ++obj[c] || 1;
+  });
+
+  for (const char of String(num2)) {
+    if (!obj[char]) return false;
+    obj[char]--;
+  }
+  return true;
+}
+
+console.log(sameFrequency1(3242342, 3242342));
+
 function sameFrequency(num1, num2) {
-  const str1 = `${num1}`;
+  const str1 = `${num1}`; // converting to string
   const str2 = `${num2}`;
 
   if (str1.length !== str2.length) return false;
 
   const obj = {};
-
+  // same logic as for finding the anagrams
   for (const char of str1) {
     obj[char] = ++obj[char] || 1;
   }
